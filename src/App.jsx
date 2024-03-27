31facc860c7832439444aa389c5c41b50115df44
Vaibhav Bijwe <vaibhavbijwe30@gmail.com>
2024-03-27 23:40:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <div className='w-full h-screen bg-black text-white '>
+    <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='w-full px-20 py-8'>
+        <img className='w-40 h-16 fill-none ' src="/public/text.png" alt="" />
+        <div className='link'>
+        <a className='text-md'>For Engineer</a>
+        </div>
+
+    </div>
+  )
+}
